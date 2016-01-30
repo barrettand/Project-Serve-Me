ea39a32cc3d4d97faa397965ba17ef5005f33b12
barrettand <barrett.and@husky.neu.edu>
2016-01-30 03:29:36 -0500

    Second Commit
    N' Junk

---
+++ Project Serve Me/Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			rigidbody2D.transform.Translate(new Vector2(0, .1f));
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			rigidbody2D.transform.Translate(new Vector2(0, -0.1f));
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			rigidbody2D.transform.Translate(new Vector2(0.1f, 0));
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			rigidbody2D.transform.Translate(new Vector2(-0.1f, 0));
+		}
+	
+	}
+}
---
+++ Project Serve Me/Assets/StickPointer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickPointer : MonoBehaviour {
+	public GameObject stick;
+	public GameObject player;
+	public int rotationOffset = 90;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(player.transform.position, Input.mousePosition) < Vector3.Distance(stick.transform.position, player.transform.position)) {
+						stick.transform.position = Camera.main.ScreenToWorldPoint (Input.mousePosition) + new Vector3 (0, 0, 10);
+				} else {
+				
+						Vector3 difference = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
+						difference.Normalize ();
+						float rotZ = Mathf.Atan2 (difference.y, difference.x) * Mathf.Rad2Deg;
+						transform.rotation = Quaternion.Euler (0f, 0f, rotZ + rotationOffset);
+				}
+	}
+}
